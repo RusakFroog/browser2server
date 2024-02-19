166c8783b29d846eb3917f1f0f099233ffd92a7a
Yiin <stanislovas.janonis@gmail.com>
2024-02-19 21:24:47 +0200

    Simplify event forwarding

--- client/index.js
+++ client/index.js
@@ -6,14 +6,7 @@ class BrowserToServer {
      * @param {string} [callName=browser.emitServer] name of event need to call from browser
     */
     initView(view, callName = "browser.emitServer") {
-        view.on(callName, function() {
-            const args = [...arguments];
-            const eventName = args[0];
-            
-            args.shift();
-            
-            altClient.emitServer(eventName, args);
-        });
+        view.on(callName, altClient.emitServer);
     }
 }
 
